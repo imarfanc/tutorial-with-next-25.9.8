c83767eb821731746df01a23af3a4d887be4b37c
imarfanc <142709234+imarfanc@users.noreply.github.com>
2025-09-08 22:22:08 -0500

    removed tutorial

--- app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  ui: {
-    primary: 'blue',
-    gray: 'neutral',
-    container: {
-      constrained: 'max-w-4xl'
-    }
-  }
-})
--- server/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { randomUUID } from 'uncrypto'
-
-export default eventHandler((event) => {
-  const listId = randomUUID()
-  return sendRedirect(event, `/${listId}`)
-})
